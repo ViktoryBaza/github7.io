597de772fec899bac310ed88638bb2f34cd3c221
Grony <gr0ny.0017@gmail.com>
2022-11-06 23:41:41 +0300

    Фикс

--- lab6.js
+++ lab6.js
@@ -1,43 +1,49 @@
-var radio = document.getElementsByName('prim');
-
-for (var i=0; i<radio.length; i++) {
-    radio[i].onchange = testRadio;
-}
+const radio = document.getElementsByName('prim');
 
 function testRadio(){
     console.log (this.value);
 }
 
-document.getElementById('one').onclick = checkRadio;
+const rama = document.querySelector('.rama');
+const one = document.getElementById('one');
+const activeImg = rama.querySelector('#activeImg');
 
-function checkRadio(){
-    var mimg=document.createElement("img");
-    mimg.setAttribute('src', '1646418281_1-abrakadabra-fun-p-chernii-krug-na-belom-3.jpg'); 
-    var mimg2=document.createElement("img");
-    mimg2.setAttribute('src', '1640961014_9-abrakadabra-fun-p-na-chernom-fone-belii-kvadratik-19.jpg'); 
-    var mimg3=document.createElement("img");
-    mimg3.setAttribute('src', '1644482597_1-abrakadabra-fun-p-chernoe-serdtse-na-prozrachnom-fone-4.jpg'); 
-
-    var printBlock = document.getElementById("printBlock");
-    var m = document.getElementsByName('prim');
-    for( var i=0; i<m.length; i++){
-        if ( m[0].checked ) {
-            alert( m[0].value );
-            printBlock.appendChild(mimg);
-            break;
-        }
-        else if ( m[1].checked ) {
-            alert( m[1].value );
-            printBlock.appendChild(mimg2);
-            break;
-        }
-        else if ( m[2].checked ) {
-            alert( m[2].value );
-            printBlock.appendChild(mimg3);
-            break;
-        }
-    }
+one.onclick = function () {
+    const figure = rama.querySelector('input[name="prim"]:checked').value;
+    activeImg.src = `${figure}.jpg`
+    alert(`Поздравляю, вы выбрали ${figure}.`)
 }
+
+
+
+// function checkRadio(){
+//     var mimg=document.createElement("img");
+//     mimg.setAttribute('src', 'круг.jpg');
+//     var mimg2=document.createElement("img");
+//     mimg2.setAttribute('src', 'квадрат.jpg');
+//     var mimg3=document.createElement("img");
+//     mimg3.setAttribute('src', 'сердце.jpg');
+//
+//     var printBlock = document.getElementById("printBlock");
+//     var m = document.getElementsByName('prim');
+//     for( var i=0; i<m.length; i++){
+//         if ( m[0].checked ) {
+//             alert( m[0].value );
+//             printBlock.appendChild(mimg);
+//             break;
+//         }
+//         else if ( m[1].checked ) {
+//             alert( m[1].value );
+//             printBlock.appendChild(mimg2);
+//             break;
+//         }
+//         else if ( m[2].checked ) {
+//             alert( m[2].value );
+//             printBlock.appendChild(mimg3);
+//             break;
+//         }
+//     }
+// }
 
 
 
@@ -61,18 +67,18 @@ function checkRadio2(){
             var elems = document.getElementsByTagName('img');
             for(var i=0; i<elems.length; i++) elems[i].style.width='40px';
             break;
-        } 
+        }
         else if ( m2[1].checked ) {
             var elems = document.getElementsByTagName('img');
             for(var i=0; i<elems.length; i++) elems[i].style.width='100px';
             break;
-        } 
+        }
         else if ( m2[2].checked ) {
             var elems = document.getElementsByTagName('img');
             for(var i=0; i<elems.length; i++) elems[i].style.width='200px';
             break;
-        } 
-        
+        }
+
     }
 }
 
@@ -96,35 +102,35 @@ function checkRadio3(){
         if ( m3[0].checked ) {
             var elems = document.getElementsByTagName('img');
             for(var i=0; i<elems.length; i++) {
-                elems[i].style.position = "absolute";
-                elems[i].style.right = "700px";
+                elems[i].style.left = '0'
+                elems[i].style.transform = 'translateX(0)';
             }
             break;
-        } 
+        }
         else if ( m3[1].checked ) {
             var elems = document.getElementsByTagName('img');
             for(var i=0; i<elems.length; i++) {
-                elems[i].style.position = "absolute";
-                elems[i].style.right = "600px";
+                elems[i].style.left = '50%'
+                elems[i].style.transform = 'translateX(-50%)';
             }
             break;
-        } 
+        }
         else if ( m3[2].checked ) {
             var elems = document.getElementsByTagName('img');
             for(var i=0; i<elems.length; i++) {
-                elems[i].style.position = "absolute";
-                elems[i].style.left = "700px";
+                elems[i].style.left = '100%'
+                elems[i].style.transform = 'translateX(-100%)';
             }
             break;
-        } 
-        
+        }
+
     }
 }
 
 
 const getAge = form => {
     let date = form.date.value;
-    
+
     let age = ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
-    form.res.value = age;        
-}  
+    form.res.value = age;
+}
